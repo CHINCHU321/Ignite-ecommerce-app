99cf38ca97b7f7fc4d5ab6de09dd6435d37d7984
Chinchu Babu <chinchubabuofficial@gmail.com>
2024-06-23 13:43:21 +0400

    commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import './assets/styles/Navbar.css';
 import SignupPage from './pages/SignupPage';
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
         <Route exact path="/" element={<Home/>}/>
         <Route path="/Signup" element={<SignupPage/>} />
+        <Route path="/Login" element={<Login/>}/>
         </Routes>
      </div>
     </div>
---
+++ src/components/Login.js
@@ -0,0 +1,47 @@
+import React, { useState, useContext } from 'react';
+import {  useNavigate } from 'react-router-dom';
+import AuthContext from '../context/AuthContext';
+import '../assets/styles/Auth.css';
+
+function Login() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle login logic
+    login(formData);
+    navigate('/');
+  };
+
+  return (
+    <div className="auth-form">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email
+          <input type="email" name="email" value={formData.email} onChange={handleChange} />
+        </label>
+        <label>
+          Password
+          <input type="password" name="password" value={formData.password} onChange={handleChange} />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
